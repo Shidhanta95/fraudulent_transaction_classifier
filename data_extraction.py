7392a83a07c73838d08f1e52a316b4ce15a1fbd4
Shidhanta95 <147026855+Shidhanta95@users.noreply.github.com>
2023-10-22 19:58:31 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    data_raw = pd.read_csv('data.csv')
+    data = pd.read_csv('data.csv')
     #data = data_raw.iloc[:100,:]
     print(data.head())
     return data
